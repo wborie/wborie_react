37127eed266891eb4df6c4139f27fee8a3777016
Will Borie <borie@usc.edu>
2018-09-25 22:47:49 -0700

    Updated the home page with a new background and short summary

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Background1 from '../../Assets/Images/Me.jpg';
+import Background1 from '../../Assets/Images/Motherboard.jpg';
+import Me from '../../Assets/Images/Me2.jpeg';
 
 class Homepage extends Component {
 
@@ -7,15 +8,14 @@ class Homepage extends Component {
         return (
             <div className="container-fluid">
                 <img src = {Background1} style={{position:"fixed", top:"80px", left:"0%", width:"100%", height:"100%",zIndex:"-1"}}/>
-                <div style= {{width:"400px",textAlign:"center"}}>
-                    <p style = {{fontSize: "40px",fontFamily: "Georgia", fontWeight:"500", color:"white", 
-                    lineHeight:"60px", position:"fixed",top:"15%",left:"15%",width:"400px"}}>
-                    Will Borie
-                    </p>
-                    <p style = {{fontSize: "40px",fontFamily: "Georgia", fontWeight:"500", color:"white", 
-                    lineHeight:"60px", position:"fixed",top:"15%",left:"45%",width:"600px"}}>
-                    Computer Science Student at USC
-                    </p>
+                <div style={{position:"fixed", top:"160px", left:"60%", textAlign:"center", width:"400px"}}>
+                    <img src = {Me} style={{width:"250px", height:"300px", border:"3px solid white"}} />
+                    <div style={{position:"absolute", top:"340px"}}>
+                        <p style = {{fontSize: "30px",fontFamily: "Georgia", fontWeight:"500", color:"#ED2A72", 
+                        lineHeight:"50px", backgroundColor:"black", border:"3px solid white"}}>
+                        Hello! My name is Will and I'm a Computer Science student at USC.
+                        </p>
+                    </div> 
                 </div>
             </div>
         );
